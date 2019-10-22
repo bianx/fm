d7455c236a606ed2b9dc6e0711e3945a6581ff17
Sergey Litvinov <slitvinov@gmail.com>
2019-10-22 19:54:50 +0000

    add cubtri_c

---
+++ lib/cubtri_c.c
@@ -0,0 +1,121 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <real.h>
+#include <co/tri.h>
+#include <co/err.h>
+#include <co/memory.h>
+
+#include "cubtri.h"
+#define T Cubtri
+
+static real U[] = {
+    0.659027622374092,
+    0.659027622374092,
+    0.231933368553031,
+    0.231933368553031,
+    0.109039009072877,
+    0.109039009072877,
+};
+
+static real V[] = {
+    0.231933368553031,
+    0.109039009072877,
+    0.659027622374092,
+    0.109039009072877,
+    0.659027622374092,
+    0.231933368553031,
+};
+
+static real W[] = {
+    0.16666666666666666667,
+    0.16666666666666666667,
+    0.16666666666666666667,
+    0.16666666666666666667,
+    0.16666666666666666667,
+    0.16666666666666666667,
+};
+
+struct T {
+
+};
+
+struct Param {
+    const real *a, *b, *c;
+     real(*f) (real, real, real, void *);
+    void *param;
+};
+
+static real
+F(real u, real v, void *vp)
+{
+    enum { X, Y, Z };
+    struct Param *p;
+    real ax, ay, az, bx, by, bz, cx, cy, cz;
+    real x, y, z;
+
+    p = vp;
+    ax = p->a[X];
+    ay = p->a[Y];
+    az = p->a[Z];
+
+    bx = p->b[X];
+    by = p->b[Y];
+    bz = p->b[Z];
+
+    cx = p->c[X];
+    cy = p->c[Y];
+    cz = p->c[Z];
+
+    x = (cx - ax) * v + (bx - ax) * u + ax;
+    y = (cy - ay) * v + (by - ay) * u + ay;
+    z = (cz - az) * v + (bz - az) * u + az;
+
+    return p->f(x, y, z, p->param);
+}
+
+
+int
+cubtri_ini(T ** pq)
+{
+    T *q;
+    int status;
+
+    MALLOC(1, &q);
+    *pq = q;
+    return CO_OK;
+}
+
+int
+cubtri_fin(T * q)
+{
+    FREE(q);
+    return CO_OK;
+}
+
+int
+cubtri_apply(T * q, const real a[3], const real b[3],
+                    const real c[3], real(*f) (real, real, real, void *),
+                    void *param, /**/ real * pres)
+{
+    struct Param p;
+    real A, u, v, w, res;
+    int i, status;
+
+    p.f = f;
+    p.param = param;
+    p.a = a;
+    p.b = b;
+    p.c = c;
+    A = tri_area(a, b, c);
+
+    res = 0;
+    for (i = 0; i < sizeof(W)/sizeof(W[0]); i++) {
+	u = U[i];
+	v = V[i];
+	w = W[i];
+	res += w * F(v, u, &p);
+    }
+    res *= A;
+    *pres = res;
+    return CO_OK;
+}
